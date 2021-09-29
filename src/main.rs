bbe7927b42ec2132e729e06a5577faedd5b25792
kofi <otuokofi@outlook,com>
2021-09-29 07:58:49 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut buf = [0; 1];
+        loop {
+            io::stdin()
+                .read_exact(&mut buf)
+                .expect("Failed to read line");
+        }
+    }
 }
