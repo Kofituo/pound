19e2b506eca3d926d3c7e550e679f7007f72a562
kofi <otuokofi@outlook,com>
2021-09-29 09:06:21 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ use std::io::Read;
 fn main() {
     /* add the following */
     let mut buf = [0; 1];
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 {}
+    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
     /* end */
 }
