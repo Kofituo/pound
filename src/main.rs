e9b8e2a6592995dd5e895792b2f70a96878410dd
kofi <otuokofi@outlook,com>
2021-09-29 09:08:26 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-/* add the following */
 use std::io;
 use std::io::Read;
-/* end */
 
 fn main() {
-    /* add the following */
     let mut buf = [0; 1];
+    /*modify*/
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
-    /* end */
+    /*end*/
 }
