462a584fa64cb4cb9f26ddffea3184ec4f4b6149
kofi <otuokofi@outlook,com>
2021-09-29 11:01:32 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use crossterm::terminal; /* add this line */
+use crossterm::terminal;
 use std::io;
 use std::io::Read;
+
+struct CleanUp;
+
+impl Drop for CleanUp {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().expect("Could not disable raw mode")
+    }
+}
+
 fn main() {
+    let _clean_up = CleanUp;
     terminal::enable_raw_mode().expect("Could not turn on Raw mode");
     let mut buf = [0; 1];
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
-    terminal::disable_raw_mode().expect("Could not turn off raw mode"); /* add this line */
+    panic!(); /* add this line*/
+    //terminal::disable_raw_mode().expect("Could not turn off raw mode"); /* comment out this line */
 }
