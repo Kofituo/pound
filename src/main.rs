4d8531994274217239e85f58859e338bba46ce19
kofi <otuokofi@outlook,com>
2021-09-29 10:42:53 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use crossterm::terminal; /* add this line */
 use std::io;
 use std::io::Read;
 fn main() {
-    terminal::enable_raw_mode().expect("Could not turn on Raw mode"); /* add this line */
+    terminal::enable_raw_mode().expect("Could not turn on Raw mode");
     let mut buf = [0; 1];
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
+    terminal::disable_raw_mode().expect("Could not turn off raw mode"); /* add this line */
 }
