ba542d9eb7d2d401cb43b5fde07f192af2ae019d
kofi <otuokofi@outlook,com>
2021-10-04 19:44:26 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,13 @@ impl Output {
         let screen_rows = self.win_size.1;
         for i in 0..screen_rows {
             self.editor_contents.push('~');
+            //add the following
+            queue!(
+                self.editor_contents,
+                terminal::Clear(ClearType::UntilNewLine)
+            )
+            .unwrap();
+            //end
             if i < screen_rows - 1 {
                 self.editor_contents.push_str("\r\n");
             }
@@ -85,18 +92,10 @@ impl Output {
     }
 
     fn refresh_screen(&mut self) -> crossterm::Result<()> {
-        queue!(
-            self.editor_contents,
-            cursor::Hide, //add this
-            terminal::Clear(ClearType::All),
-            cursor::MoveTo(0, 0)
-        )?;
+        //modify
+        queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))?;
         self.draw_rows();
-        queue!(
-            self.editor_contents,
-            cursor::MoveTo(0, 0),
-            /* add this */ cursor::Show
-        )?;
+        queue!(self.editor_contents, cursor::MoveTo(0, 0), cursor::Show)?;
         self.editor_contents.flush()
     }
 }
